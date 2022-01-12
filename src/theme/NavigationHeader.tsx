72e4fbebdacd23cbc41ce07904e32b0d8fa9d546
ww8007 <wshmin@naver.com>
2022-01-12 19:04:33 +0900

    Feat: add NavigationHedaer theme

---
+++ src/theme/NavigationHeader.tsx
@@ -0,0 +1,82 @@
+import React from 'react';
+import type { FC, ReactNode } from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+import type { StyleProp, ViewStyle, TextStyle } from 'react-native';
+import { Colors } from 'react-native-paper';
+
+export type NavigationHeaderProps = {
+	title?: string;
+	Left?: () => ReactNode;
+	Right?: () => ReactNode;
+	secondRight?: () => ReactNode;
+	thirdRight?: () => ReactNode;
+	headerColor?: string;
+	viewStyle?: StyleProp<ViewStyle>;
+	titleStyle?: StyleProp<TextStyle>;
+};
+
+export const NavigationHeader: FC<NavigationHeaderProps> = ({
+	title,
+	Left,
+	Right,
+	secondRight,
+	thirdRight,
+	viewStyle,
+	titleStyle,
+	headerColor,
+}) => {
+	return (
+		<View
+			style={[
+				styles.view,
+				viewStyle,
+				{ backgroundColor: headerColor ? headerColor : '#33aafc' },
+			]}
+		>
+			<View style={{ flex: 0.1, backgroundColor: 'transparent' }}></View>
+			<View style={styles.flex}>{Left && Left()}</View>
+			<View style={styles.flex}>
+				<Text style={[styles.title, titleStyle]}>{title}</Text>
+			</View>
+			<View
+				style={[
+					styles.flex,
+					{ flexDirection: 'row', justifyContent: 'flex-end' },
+				]}
+			>
+				{thirdRight && thirdRight()}
+				<View style={{ flex: 0.2, backgroundColor: 'transparent' }}></View>
+				{secondRight && secondRight()}
+				<View style={{ flex: 0.2, backgroundColor: 'transparent' }}></View>
+				{Right && Right()}
+			</View>
+			<View style={{ flex: 0.1, backgroundColor: 'transparent' }}></View>
+		</View>
+	);
+};
+
+const styles = StyleSheet.create({
+	flex: { flex: 1, backgroundColor: 'transparent' },
+	view: {
+		width: '100%',
+		padding: 9,
+		flexDirection: 'row',
+		alignItems: 'center',
+		justifyContent: 'space-around',
+		backgroundColor: '#33aafc',
+
+		height: 50,
+	},
+	title: {
+		marginTop: 3,
+		fontSize: 19,
+		fontWeight: '300',
+		textAlign: 'center',
+
+		fontFamily: 'NanumSquareBold',
+		letterSpacing: -2,
+		color: '#fff',
+	},
+	// flex: { flex: 1, backgroundColor: 'transparent' },
+});
