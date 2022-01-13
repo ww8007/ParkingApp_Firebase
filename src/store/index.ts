a20cea0ed25b24ce26ab830c069e5081fced208b
ww8007 <wshmin@naver.com>
2022-01-13 22:10:51 +0900

    Feat: add loginSaga, list reducer

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,15 +1,20 @@
 import { combineReducers } from 'redux';
-
+import { all } from 'redux-saga/effects';
 import { persistReducer } from 'redux-persist';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import login from './login';
-
+import login, { loginSaga } from './login';
+import car, { carSaga } from './car';
+import list from './list';
 // import storageSession from 'redux-persist/lib/storage/session';
 const persistConfig = {
 	key: 'root',
 	storage: AsyncStorage,
-	blacklist: ['login'],
+	blacklist: ['car'],
 };
-const rootReducer = combineReducers({ login });
+const rootReducer = combineReducers({ login, car, list });
 export default persistReducer(persistConfig, rootReducer);
 export type RootState = ReturnType<typeof rootReducer>;
+
+export function* rootSaga() {
+	yield all([loginSaga(), carSaga()]);
+}
