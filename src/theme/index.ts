82354457c93b19f4124aeb72d9f5ed79a5f250e2
ww8007 <wshmin@naver.com>
2022-01-13 22:08:15 +0900

    Feat: add export * from CloseButton, ModalView, Sequence

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,2 +1,6 @@
 export * from './SafeAreaView';
 export * from './NavigationHeader';
+export * from './Button';
+export * from './CloseButton';
+export * from './ModalView';
+export * from './Sequence';
