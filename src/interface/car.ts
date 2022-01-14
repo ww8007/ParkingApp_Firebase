cd105ff0ff298659f1f174a12d0d0682d5350d59
ww8007 <wshmin@naver.com>
2022-01-14 22:42:58 +0900

    Types: add car, postDiscountAPI interface

---
+++ src/interface/car.ts
@@ -0,0 +1,13 @@
+export interface car {
+	error: string;
+	carList: any;
+	carInfo: any;
+	entryDate: string;
+	carId: string;
+}
+
+export interface postDiscountAPI {
+	carNum: number;
+	discountType: number;
+	carNo: number;
+}
