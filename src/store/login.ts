49c8e21bd721e4fb2fd1836bdb82418fe0d11292
ww8007 <wshmin@naver.com>
2022-01-15 19:29:44 +0900

    Feat: add setUserInfo, setPushToken actions

--- src/store/login.ts
+++ src/store/login.ts
@@ -4,18 +4,33 @@ import { getCarNumber } from '../lib/api/login';
 import createRequestThunk from '../lib/util/createRequestThunk';
 import { AppThunk } from './store';
 import * as api from '../lib/api/login';
-
-const initialState = {
+import createRequestSaga from '../lib/util/createRequestSaga';
+import { takeLatest } from 'redux-saga/effects';
+import type { login, personalInfo, serverInfo } from '../interface/login';
+import car from './car';
+const initialState: login = {
 	data: {},
+	error: '',
+	uid: '',
+	email: '',
+	carNum: '',
+	name: '',
+	pushToken: '',
 };
 
 const GET_CAR = 'login/GET_CAR';
+const GET_LOGIN = 'login/GET_LOGIN';
 
-export const getCar = createAction(GET_CAR, (data: any) => data);
+export const getCar = createAction(GET_CAR);
+export const getLogin = createAction(GET_LOGIN);
 
-const getCarThunk = createRequestThunk(GET_CAR, api.getCarNumber);
+const getCarSaga = createRequestSaga(GET_CAR, api.getCarNumber);
+const getLoginSaga = createRequestSaga(GET_LOGIN, api.getLogin);
 
-export function loginThunk() {}
+export function* loginSaga() {
+	yield takeLatest(GET_CAR, getCarSaga);
+	yield takeLatest(GET_LOGIN, getLoginSaga);
+}
 
 const loginSlice = createSlice({
 	name: 'login',
@@ -23,14 +38,34 @@ const loginSlice = createSlice({
 	reducers: {
 		GET_CAR_SUCCESS: (state, action: PayloadAction<any>) => {
 			state.data = action.payload;
+			console.log(action.payload);
 		},
 		GET_CAR_FAILURE: (state, action: PayloadAction<any>) => {
+			state.error = action.payload;
+		},
+		GET_LOGIN_SUCCESS: (state, action: PayloadAction<any>) => {
 			state.data = action.payload;
+		},
+		GET_LOGIN_FAILURE: (state, action: PayloadAction<any>) => {
+			state.error = action.payload;
+		},
+		setUid: (state, action: PayloadAction<serverInfo>) => {
+			const { uid, email } = action.payload;
+			state.uid = uid;
+			state.email = email;
+		},
+		setUserInfo: (state, action: PayloadAction<personalInfo>) => {
+			const { name, carNum } = action.payload;
+			state.name = name;
+			state.carNum = carNum;
+		},
+		setPushToken: (state, action: PayloadAction<string>) => {
+			state.pushToken = action.payload;
 		},
 	},
 });
 
-export const { GET_CAR_FAILURE, GET_CAR_SUCCESS } = loginSlice.actions;
+export const { setUid, setUserInfo, setPushToken } = loginSlice.actions;
 
 export default loginSlice.reducer;
 
