c8e2ccbadf3eb2226da3d70a77eb26c63d95ef66
ww8007 <wshmin@naver.com>
2022-01-17 22:26:31 +0900

    Types: add carInfoState, postDiscountAPI interface

--- src/interface/car.ts
+++ src/interface/car.ts
@@ -1,13 +1,36 @@
 export interface car {
 	error: string;
 	carList: any;
-	carInfo: any;
+	loginInfo: any;
+	carNum: string;
+	carInfo: carInfo;
+	isCarGetSuccess: boolean;
+	isPostSuccess: boolean;
+}
+
+interface carInfo {
+	entryDateString: string;
 	entryDate: string;
-	carId: string;
+	id: string;
+	differentTime: string;
 }
 
 export interface postDiscountAPI {
-	carNum: number;
+	id: string;
 	discountType: number;
-	carNo: number;
+	carNo: string;
+}
+
+export interface discountType {
+	under3: 1;
+	under5: 4;
+	over5: 7;
+}
+
+export interface carInfoState {
+	carNum: string;
+	email: string;
+	hour: string;
+	timeStamp: string;
+	name: string;
 }
