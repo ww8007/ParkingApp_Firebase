6c1246065a7be93497ce721075efd3464869e0ab
ww8007 <wshmin@naver.com>
2022-01-21 22:48:02 +0900

    Fix: remove headlessCheck

--- index.js
+++ index.js
@@ -13,17 +13,8 @@ messaging().setBackgroundMessageHandler(async (remoteMessage) => {
 	Alert.alert('good');
 });
 
-function HeadlessCheck({ isHeadless }) {
-	if (isHeadless) {
-		// App has been launched in the background by iOS, ignore
-		return null;
-	}
-
-	return <App />;
-}
-
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
 registerRootComponent(App);
-AppRegistry.registerComponent('app', () => HeadlessCheck);
+AppRegistry.registerComponent('app', () => App);
