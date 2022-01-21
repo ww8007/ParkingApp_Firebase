91e9b10143c6bccb657ccd1ad1cb9da396cb5c62
ww8007 <wshmin@naver.com>
2022-01-21 22:50:57 +0900

    Feat: add setToday, updateList action Fix: addList action to check isCanSubmit logic to Alert

--- src/store/list.ts
+++ src/store/list.ts
@@ -1,33 +1,63 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-import type { list, addListType } from '../interface/list';
+import type { list, addListType, addTimeType } from '../interface/list';
+import dayjs from 'dayjs';
+import 'dayjs/locale/ko';
+import { Alert } from 'react-native';
+
+dayjs().locale('ko');
+
 const initialState: list = {
 	timeList: [],
 	idxNumber: 0,
+	today: '',
+	isCanRequest: true,
+	isCanSubmit: false,
 };
 
 const listSlice = createSlice({
 	name: 'list',
 	initialState,
 	reducers: {
-		addList: (state, action: PayloadAction<string>) => {
-			const data = {
-				time: action.payload,
-				status: 'send',
-				id: state.idxNumber,
-			};
-			state.idxNumber = state.idxNumber + 1;
-			state.timeList = [data, ...state.timeList];
+		setToday: (state, action: PayloadAction<string>) => {
+			state.today = action.payload;
+		},
+		addList: (state, action: PayloadAction<addTimeType>) => {
+			if (state.isCanRequest === true) {
+				const data = {
+					time: action.payload.time,
+					status: 'send',
+					id: state.idxNumber,
+					day: action.payload.day,
+				};
+				state.idxNumber = state.idxNumber + 1;
+				state.timeList = [data, ...state.timeList];
+				state.isCanSubmit = true;
+			} else {
+				Alert.alert('알림', '하루에 한 번만 신청이 가능 합니다');
+				state.isCanSubmit = false;
+			}
+
+			state.isCanRequest = false;
 		},
 		removeList: (state, action: PayloadAction<number>) => {
-			console.log(action.payload);
-			console.log(state.timeList);
+			const find = state.timeList.find((item) => item.day === state.today);
+			if (find) {
+				state.isCanRequest = true;
+				state.isCanSubmit = false;
+			}
 			state.timeList = state.timeList.filter(
 				(item) => item.id !== action.payload
 			);
 		},
+		updateList: (state, action: PayloadAction<string>) => {
+			const find = state.timeList.find((item) => item.day === state.today);
+			if (find) {
+				find.status = action.payload;
+			}
+		},
 	},
 });
 
-export const { addList, removeList } = listSlice.actions;
+export const { addList, removeList, setToday, updateList } = listSlice.actions;
 
 export default listSlice.reducer;
