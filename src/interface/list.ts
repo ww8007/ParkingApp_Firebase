88491724a57a69f033aa13a8bb8cff9f904a6566
ww8007 <wshmin@naver.com>
2022-01-22 20:48:52 +0900

    Types: add todya, isCanRequest, isCanSubmit type to list interface

--- src/interface/list.ts
+++ src/interface/list.ts
@@ -1,10 +1,19 @@
 export interface list {
 	timeList: addListType[];
 	idxNumber: number;
+	today: string;
+	isCanRequest: boolean;
+	isCanSubmit: boolean;
 }
 
 export interface addListType {
 	time: string;
 	status: string;
 	id: number;
+	day: string;
+}
+
+export interface addTimeType {
+	day: string;
+	time: string;
 }
