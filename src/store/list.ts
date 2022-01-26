b5ec0c461bce23f7f8884f39225394c4773f2236
ww8007 <wshmin@naver.com>
2022-01-26 13:45:24 +0900

    Feat: add check isCanExtraRequest for extra request

--- src/store/list.ts
+++ src/store/list.ts
@@ -12,6 +12,7 @@ const initialState: list = {
 	today: '',
 	isCanRequest: true,
 	isCanSubmit: false,
+	isCanExtraRequest: false,
 };
 
 const listSlice = createSlice({
@@ -22,28 +23,37 @@ const listSlice = createSlice({
 			state.today = action.payload;
 		},
 		addList: (state, action: PayloadAction<addTimeType>) => {
-			if (state.isCanRequest === true) {
+			if (state.isCanRequest === true || state.isCanExtraRequest === true) {
 				const data = {
 					time: action.payload.time,
 					status: 'send',
 					id: state.idxNumber,
 					day: action.payload.day,
+					postHour: action.payload.postHour,
 				};
 				state.idxNumber = state.idxNumber + 1;
 				state.timeList = [data, ...state.timeList];
+				if (state.isCanRequest === false) state.isCanExtraRequest = false;
 				state.isCanSubmit = true;
 			} else {
 				Alert.alert('알림', '하루에 한 번만 신청이 가능 합니다');
 				state.isCanSubmit = false;
 			}
-
-			state.isCanRequest = false;
+			// 추가 신청 설정
+			if (action.payload.postHour === '3' || action.payload.postHour === '0') {
+				state.isCanExtraRequest = true;
+				state.isCanRequest = false;
+			} else {
+				state.isCanExtraRequest = false;
+				state.isCanRequest = false;
+			}
 		},
 		removeList: (state, action: PayloadAction<number>) => {
 			const find = state.timeList.find((item) => item.day === state.today);
 			if (find) {
 				state.isCanRequest = true;
 				state.isCanSubmit = false;
+				state.isCanExtraRequest = false;
 			}
 			state.timeList = state.timeList.filter(
 				(item) => item.id !== action.payload
@@ -55,9 +65,15 @@ const listSlice = createSlice({
 				find.status = action.payload;
 			}
 		},
+		dayChange: (state) => {
+			state.isCanRequest = true;
+			state.isCanExtraRequest = false;
+			state.isCanSubmit = false;
+		},
 	},
 });
 
-export const { addList, removeList, setToday, updateList } = listSlice.actions;
+export const { addList, removeList, setToday, updateList, dayChange } =
+	listSlice.actions;
 
 export default listSlice.reducer;
